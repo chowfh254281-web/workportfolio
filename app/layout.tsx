1afcc55598cb755c663db409e1cb898c8ac2f3c9
Sam Chow <chowfh254281@gmail.com>
2026-01-22 11:07:19 +0800

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,28 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+// 1. 引入 GoogleAnalytics 组件
+import { GoogleAnalytics } from '@next/third-parties/google';
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Sam Chow Portfolio',
-  description: 'Multimedia Designer Work Portfolio',
-}
+  title: "Sam Chow Portfolio",
+  description: "Multimedia Designer Portfolio",
+};
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>
+        {children}
+      </body>
+      {/* 2. Google Analytics 設定 - 已填入你的 ID */}
+      <GoogleAnalytics gaId="G-KH8NZMDX59" />
     </html>
-  )
+  );
 }
